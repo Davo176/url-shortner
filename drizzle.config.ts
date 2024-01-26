f11c1621478d6334c98056e4fee2cc73ee2d24d1
Davo176 <wil1234@live.com.au>
2024-01-26 17:37:49 +1030

    fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,9 +5,9 @@ export default {
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    host: process.env.DB_HOST || "",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || "",
+    host: process.env.PGHOST || "",
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE || "",
   },
 } satisfies Config;
